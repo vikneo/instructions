98ffb836815c879c4fcef4a3c277bd275976f79a
Viktor <vm-viktor@mail.ru>
2024-10-02 16:54:27 +0700

    added model the Settings

--- documents/instruction/models.py
+++ documents/instruction/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 
-class Brand:
+class Brand(models.Model):
     """
     The class describes the Brand model
     """
     name = models.CharField(max_length=100, verbose_name='Brand', db_index=True)
     slug = models.SlugField(max_length=100, verbose_name='URL')
-    device_id = models.ForeignKey("Device", on_delete=models.CASCADE, verbose_name='Device', related_name='devices')
 
     def __str__(self) -> str:
         return self.name
@@ -21,16 +20,14 @@ class Brand:
         verbose_name_plural = 'brands'
     
 
-class Device:
+class Device(models.Model):
     """
     The class describes the Device model
     """
+    brand_id = models.ForeignKey(Brand, on_delete=models.CASCADE, verbose_name='Device', related_name='brands')
     name = models.CharField(max_length=150, verbose_name='Device', db_index=True)
-    description = models.TextField(verbose_name='Description', blank=True, default=' ')
     slug = models.SlugField(max_length=150, verbose_name='URL')
-    files_id = models.ForeignKey("Files", on_delete=models.CASCADE, verbose_name='Photo', related_name='files')
-    network_id = models.ForeignKey("Network", on_delete=models.CASCADE, verbose_name='Netork', related_name='networks')
-    instruction_file_id = models.OneToOneField("InstructionFile", on_delete=models.CASCADE, verbose_name='Instruction', related_name='instructions')
+    description = models.TextField(verbose_name='Description', blank=True, default=' ')
 
 
     def __str__(self) -> str:
@@ -42,10 +39,11 @@ class Device:
         verbose_name_plural = 'devices'
 
 
-class InstructionFile:
+class InstructionFile(models.Model):
     """
     The class describes the InstructionFile model
     """
+    device_id = models.OneToOneField(Device, on_delete=models.CASCADE, verbose_name='Instruction', related_name='devices_instructs')
     name = models.CharField(max_length=120, verbose_name='Name', db_index=True)
     slug = models.SlugField(max_length=120, verbose_name='URL')
     description = models.TextField(verbose_name='Description', blank=True, default=' ')
@@ -59,10 +57,11 @@ class InstructionFile:
         verbose_name_plural = 'instructions'
 
 
-class File:
+class File(models.Model):
     """
     The class describes the InstructionFile model
     """
+    device_id = models.ForeignKey(Device, on_delete=models.CASCADE, verbose_name='Photo', related_name='devices_files')
     name = models.CharField(max_length=120, verbose_name='Name', db_index=True)
     slug = models.SlugField(max_length=120, verbose_name='URL')
     description = models.TextField(verbose_name='Description', blank=True, default=' ')
@@ -76,13 +75,15 @@ class File:
         verbose_name_plural = 'photos'
 
 
-class Network:
+class Network(models.Model):
     """
     The class describes the InstructionFile model
     """
+    device_id = models.ForeignKey(Device, on_delete=models.CASCADE, verbose_name='Netork', related_name='devices_networks')
     name = models.CharField(max_length=120, verbose_name='Name', db_index=True)
     slug = models.SlugField(max_length=120, verbose_name='URL')
     description = models.TextField(verbose_name='Description', blank=True, default=' ')
+    settings = models.OneToOneField("Settings", verbose_name="Settings", on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
@@ -92,4 +93,59 @@ class Network:
         verbose_name = 'network'
         verbose_name_plural = 'networks'
 
-# TODO дописать модель Settings - for config Networks
+
+class Settings(models.Model):
+    """
+    The class describes the Settings model
+    """
+    class Speed(models.TextChoices):
+        '1200'
+        '2400'
+        '4800'
+        '9600'
+        '19200'
+        '38400'
+        '57600'
+        '115200'
+
+        __empty__ = 'speed'
+
+    class Paritet(models.TextChoices):
+        EVEN = 'E'
+        NONE = 'N'
+        ODD = 'O'
+
+        __empty__ = 'paritet'
+    
+    class Bits(models.TextChoices):
+        '7'
+        '8'
+        '9'
+
+        __empty__ = 'bits'
+    
+    class StopBit(models.TextChoices):
+        '1'
+        '2'
+
+        __empty__ = 'storp bits'
+
+    mode = models.CharField(max_length=50, verbose_name='Mode', db_index=True)
+    slug = models.SlugField(max_length=50, verbose_name='URL', db_index=True)
+    slave_id = models.IntegerField(verbose_name='Slave ID')
+    speed = models.CharField(max_length=6, verbose_name='Speed', default='9600', choices=Speed)
+    paritet = models.CharField(max_length=1, verbose_name='Paritet', default='N', choices=Paritet)
+    bit = models.CharField(max_length=1, verbose_name='Bits', default='8', choices=Bits)
+    stop_bit = models.CharField(max_length=1, verbose_name='Stop bit', default='1', choices=StopBit)
+    ip_address = models.CharField(max_length=15, verbose_name='IP address')
+    mask = models.CharField(max_length=15, verbose_name='Mask')
+    gateway = models.CharField(max_length=15, verbose_name='Gateway')
+    description = models.TextField(verbose_name='Description', default=' ', blank=True)
+
+    def __str__(self) -> str:
+        return self.mode
+    
+    class Meta:
+        db_table = 'settings'
+        verbose_name = 'setting'
+        verbose_name_plural = 'settings'
