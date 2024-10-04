fdb56bc85ba424cc51b3b4cb6ddc2c9df0238f58
Viktor <vm-viktor@mail.ru>
2024-10-04 10:29:56 +0700

    added import signals

--- documents/instruction/apps.py
+++ documents/instruction/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class InstructionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'instruction'
+
+    def ready(self) -> None:
+        print("def ready(self) ")
+        import instruction.signals
