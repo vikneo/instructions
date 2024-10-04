825d31462d4bc356e84e5ab1a049443de6faba72
Viktor <vm-viktor@mail.ru>
2024-10-04 10:29:40 +0700

    created signals

---
+++ documents/instruction/signals.py
@@ -0,0 +1,20 @@
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+
+from utils.slugify import slugify
+from .models import Settings
+
+import re
+
+
+@receiver(pre_save, sender=Settings)
+def get_slugify_instruction(instance, **kwargs) -> None:
+    """
+    Before saving the model, the "slug" field is checked, 
+    if the field is empty, it is filled in from the "name" 
+    field through the "slugify" module
+    """
+    if re.search(r'[0-9]-[0-9]', instance.slug):
+        # TODO для уникальности добавить к slug id_CRM + id_project или name_project
+        instance.slug = f"{slugify(instance.interface.name)}- \
+                        {slugify(instance.device.name)}"
