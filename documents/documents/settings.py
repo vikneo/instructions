a3ae752c5dd94b5ad59106b5319be6d25fedeec4
Viktor <vm-viktor@mail.ru>
2024-10-04 14:26:37 +0700

    add settings for dir media and cach

--- documents/documents/settings.py
+++ documents/documents/settings.py
@@ -72,6 +72,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'documents.wsgi.application'
 
+CACHE_ROOT = os.path.join(BASE_DIR, "cache")
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+        "LOCATION": CACHE_ROOT,
+    }
+}
+
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
@@ -119,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
