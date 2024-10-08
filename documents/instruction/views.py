d1cfe32d4196e8e9236a0a2843e085b00fbb6300
Viktor <vm-viktor@mail.ru>
2024-10-08 16:05:03 +0700

    created view the ProjectListView

--- documents/instruction/views.py
+++ documents/instruction/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import logging
+from typing import Any
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from .models import (
+    Project,
+    Brand,
+    Device,
+    Network,
+    InstructionFile,
+    File,
+    Settings
+)
+
+logger = logging.getLogger(__name__)
+
+
+class ProjectListView(ListView):
+    """
+    The "ProjectListView" class displays a list of projects
+    """
+    model = Project
+    template_name = 'product/product_list.html'
+    context_object_name = 'products'
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+
+        context = super().get_context_data(**kwargs)
+        context.update(title='Network technologies')
+        return context
