c87262d6aac3ab30b696f65ea9cffdd0696f7c75
Viktor <vm-viktor@mail.ru>
2024-10-09 10:25:17 +0700

    added in settings.py STATICFILES_DIRS

--- documents/documents/settings.py
+++ documents/documents/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / 'static'
+LOCAL_MACHINE_DEV = os.getenv("LOCAL_MACHINE_DEV") == "1"
 
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
