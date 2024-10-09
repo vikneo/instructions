3123d07f8761861adf33c5eb3f1b0f2182429b35
Viktor <vm-viktor@mail.ru>
2024-10-09 09:25:15 +0700

    add STATIC_ROOT

--- documents/documents/settings.py
+++ documents/documents/settings.py
@@ -120,7 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
