a873a3bc32712426e57d146bec827e82632d35c1
Viktor <vm-viktor@mail.ru>
2024-10-09 16:35:57 +0700

    added path to product_detail.html

--- documents/instruction/urls.py
+++ documents/instruction/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ProjectListView
+from .views import (
+    ProjectListView,
+    ProjectDetailView
+    )
 
+app_name = 'project'
 
 urlpatterns = [
-    path('', ProjectListView.as_view(), name='products')
+    path('', ProjectListView.as_view(), name='index'),
+    path('detail/<slug:slug>', ProjectDetailView.as_view(), name='product-detail')
 ]
