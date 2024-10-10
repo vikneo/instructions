07b9af8904a709beeefc6f76e49ece07a23dcd37
vikneo <vm-viktor@mail.ru>
2024-10-10 21:44:35 +0700

    created login & logout views

--- documents/users/views.py
+++ documents/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+
+
+logger = logging.getLogger(__name__)
+
+
+class LoginUserView(LoginView):
+    """
+    Authorization user view.
+    """
+    template_name = 'account/login.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            title='Авторизация'
+        )
+        logger.info("Загружена страница '%s'", context['title'])
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('index')
+
+
+class LogoutUserView(LogoutView):
+    """
+    Logout user view.
+    """
+    next_page = reverse_lazy('project:index')
+
+    def dispatch(self, request, *args, **kwargs):
+        logger.info("User '%s' logged out", request.user)
+        return super().post(request, *args, **kwargs)
