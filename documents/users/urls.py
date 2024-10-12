2022a82bbc1c03394a5f2e4337fa681daefe42ed
vikneo <vm-viktor@mail.ru>
2024-10-12 21:55:16 +0700

    added path to register

--- documents/users/urls.py
+++ documents/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LoginUserView, LogoutUserView
+from .views import LoginUserView, LogoutUserView, RegisterUserView
 
 
 app_name = 'users'
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('login/', LoginUserView.as_view(), name='login'),
     path('logout/', LogoutUserView.as_view(), name='logout'),
+    path('register/', RegisterUserView.as_view(), name = 'register'),
 ]
