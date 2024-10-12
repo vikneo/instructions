4590e2c3626f3f16356596f9d418bf1d4cd28ad0
vikneo <vm-viktor@mail.ru>
2024-10-12 21:54:25 +0700

    created form register user in views.py

--- documents/users/forms.py
+++ documents/users/forms.py
@@ -15,3 +15,20 @@ class AuthFormUser(AuthenticationForm):
         widget=forms.PasswordInput(attrs={'class': 'form-input'})
     )
 
+
+class RegisterFormUser(UserCreationForm):
+    """
+
+    """
+    username = forms.CharField(
+        label = 'Логин',
+        widget=forms.TextInput(attrs={'class': 'form-input'})
+    )
+    password1 = forms.CharField(
+        label = 'Пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
+    )
+    password2 = forms.CharField(
+        label = 'Повторить пароль',
+        widget=forms.PasswordInput(attrs={'class': 'form-input'})
+    )
