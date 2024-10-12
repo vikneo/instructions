d318f29e4d02061cd65b16f78a88b59d3d837d92
vikneo <vm-viktor@mail.ru>
2024-10-12 21:04:58 +0700

    added jQuery

---
+++ documents/static/access/js/main.js
@@ -0,0 +1,18 @@
+let signinForm = document.querySelector('.signin-form');
+
+let qweryForm = document.querySelector(".qwery-form")
+
+function signIn() {
+
+    // $(window).load(function () {
+    //     console.log('reload')
+    //     location.reload()
+    // })
+}
+
+// function openSignIn() {
+//     console.log('Open window');
+//
+//     $("#exampleModal").load("user/login/");
+// }
+
