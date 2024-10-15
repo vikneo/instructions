d650fb25ce0d6d612b9eeb649ae21802aaa4c3d2
Viktor <vm-viktor@mail.ru>
2024-10-15 16:35:16 +0700

    added path to template searche.html

--- documents/instruction/urls.py
+++ documents/instruction/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     ProjectListView,
     ProjectDetailView,
-    DeviceDetailView
+    DeviceDetailView,
+    SearchProjectView,
     )
 
 app_name = 'project'
@@ -10,5 +11,6 @@ app_name = 'project'
 urlpatterns = [
     path('', ProjectListView.as_view(), name='index'),
     path('product/<slug:slug>/detail', ProjectDetailView.as_view(), name='product-detail'),
-    path('device/<slug:slug>/detail', DeviceDetailView.as_view(), name='device-detail')
+    path('device/<slug:slug>/detail', DeviceDetailView.as_view(), name='device-detail'),
+    path('product/searche', SearchProjectView.as_view(), name='searche'),
 ]
