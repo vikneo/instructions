83b85acfe625ec59d031cbffe3588d5ad51a519f
Viktor <vm-viktor@mail.ru>
2024-10-16 13:33:46 +0700

    created signal cleaned_cache from cleaned cache the Project

--- documents/instruction/signals.py
+++ documents/instruction/signals.py
@@ -1,10 +1,14 @@
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
+from django.core.cache import cache
 
 from utils.slugify import slugify
-from .models import Settings
+from .models import Settings, Project
 
 import re
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @receiver(pre_save, sender=Settings)
@@ -17,3 +21,12 @@ def get_slugify_instruction(instance, **kwargs) -> None:
     if re.search(r'[0-9]-[0-9]', instance.slug):
         instance.slug = f"{slugify(instance.device.name)}-{slugify(instance.device.designation)}-{slugify(instance.device.serial_num)}"
                         
+
+@receiver(pre_save, sender=Project)
+def cleaned_cache(instance, **kwargs) -> None:
+    """
+    
+    """
+    cache.delete('products')
+    logger.warning('Очищен кеш `Project`')
+
