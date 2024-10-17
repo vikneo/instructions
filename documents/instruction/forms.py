a19d3ea95c3b4ec72fca27a1455541e0c9914293
Viktor <vm-viktor@mail.ru>
2024-10-17 15:49:11 +0700

    created form the CreatedInstructionForms

--- documents/instruction/forms.py
+++ documents/instruction/forms.py
@@ -1,9 +1,47 @@
 from django import forms
 
-from models import WaveSensor
+from .models import InstructionFile
 
 
 class CSVImportForm(forms.Form):
     """ """
 
     csv_file = forms.FileField()
+
+
+class CreatedInstructionForms(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["brand_id"].empty_label = "Производители"
+        self.fields["device_id"].empty_label = "Устройство"
+
+    name = forms.CharField(
+        label="Название",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-input",
+                "id": "name",
+                "name": "name",
+                "type": "text",
+                "data-validate": "require",
+            }
+        ),
+    )
+    description = forms.CharField(
+        label="Описание",
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-input",
+                "id": "description",
+                "name": "description",
+                "type": "text",
+                "data-validate": "require",
+            }
+        ),
+    )
+    docs = forms.FileField(label="Файл")
+
+    class Meta:
+        model = InstructionFile
+        fields = ["brand_id", "device_id", "name", "description", "docs"]
