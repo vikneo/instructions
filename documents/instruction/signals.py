176b2787da5ada17c43bc9b8f61e7f12c2ac5561
Viktor <vm-viktor@mail.ru>
2024-10-17 15:48:07 +0700

    added signal for save field slug in the InstructionFile

--- documents/instruction/signals.py
+++ documents/instruction/signals.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 
 @receiver(pre_save, sender=Settings)
-def get_slugify_instruction(instance, **kwargs) -> None:
+def get_slugify_settings(instance, **kwargs) -> None:
     """
     Before saving the model, the "slug" field is checked, 
     if the field is empty, it is filled in from the "name" 
@@ -20,7 +20,18 @@ def get_slugify_instruction(instance, **kwargs) -> None:
     """
     if re.search(r'[0-9]-[0-9]', instance.slug):
         instance.slug = f"{slugify(instance.device.name)}-{slugify(instance.device.designation)}-{slugify(instance.device.serial_num)}"
-                        
+    if not instance.slug:
+        instance.slug = f"{slugify(instance.name)}"
+
+@receiver(pre_save, sender=InstructionFile)
+def get_slugify_instruction(instance, **kwargs) -> None:
+    """
+    Before saving the model, the "slug" field is checked, 
+    if the field is empty, it is filled in from the "name" 
+    field through the "slugify" module
+    """
+    if not instance.slug:
+        instance.slug = f"{slugify(instance.name)}"
 
 @receiver(pre_save, sender=Project)
 def cleaned_cache_project(instance, **kwargs) -> None:
