fc403a3069feab7be57f7e07b26ebdeb447bd9e2
Viktor <vm-viktor@mail.ru>
2024-10-22 12:56:04 +0700

    added for the CreatedDeviceForm

--- documents/instruction/forms.py
+++ documents/instruction/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import InstructionFile
+from .models import InstructionFile, Device
 
 
 class CSVImportForm(forms.Form):
@@ -45,3 +45,77 @@ class CreatedInstructionForms(forms.ModelForm):
     class Meta:
         model = InstructionFile
         fields = ["brand_id", "device_id", "name", "description", "docs"]
+
+
+class CreatedDeviceForm(forms.ModelForm):
+    """
+    
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["brand_id"].empty_label = "Производители"
+        self.fields["project_id"].empty_label = "Проекты"
+    
+    name = forms.CharField(
+        label='Наименование', 
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-input",
+                "id": "name",
+                "name": "name",
+                "type": "text",
+                "data-validate": "require",                
+            }
+            ))
+    designation = forms.CharField(
+        label='Обозначение по РКД',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-input",
+                "id": "designation",
+                "name": "designation",
+                "type": "text",
+                "data-validate": "require",
+            }
+        )
+    )
+    serial_num = forms.CharField(
+        label='Серийный номер',
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-input",
+                "id": "serial_num",
+                "name": "serial_num",
+                "type": "text",
+                "data-validate": "require",
+            }
+        )
+    )
+    description = forms.CharField(
+        label='Серийный номер',
+        widget=forms.Textarea(
+            attrs={
+                'row': 60,
+                'cols': 5,
+                "class": "form-input",
+                "id": "description",
+                "name": "description",
+                "type": "text",
+                "data-validate": "require",
+            }
+        )
+    )
+    termodate = forms.CharField(label='Термомониторинг', widget=forms.CheckboxInput())
+
+    class Meta:
+        model = Device
+        fields = [
+            'brand_id',
+            'project_id',
+            'name',
+            'designation',
+            'serial_num',
+            'description',
+            'termodate',
+            'network_id'
+        ]
