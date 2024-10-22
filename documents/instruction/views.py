fb0457843c270bf32258d17a468d60222e3a1796
Viktor <vm-viktor@mail.ru>
2024-10-22 14:16:09 +0700

    add slug in the CreateDeviceView

--- documents/instruction/views.py
+++ documents/instruction/views.py
@@ -248,7 +248,8 @@ class CreateDeviceView(CreateView):
     def  get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context.update(
-            title='Добавить устройство'
+            title='Добавить устройство',
+            slug=self.kwargs['slug']
         )
         return context
     
