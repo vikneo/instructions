232f7daafa759e718e7081ff510ebf2141d9ac6a
Viktor <vm-viktor@mail.ru>
2024-11-01 12:27:36 +0700

    added the BrandView class in views.py

--- documents/manuals/views.py
+++ documents/manuals/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+import logging
 
-# Create your views here.
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from models import (
+    Brand,
+    Module
+)
+from utils.format_name_uer import format_name
+
+logger = logging.getLogger(__name__)
+
+
+class BrandView(ListView):
+    """
+    The "BrandView" class displays a list of all manufacturers
+    """
+    model = Brand
+    template_name = 'documents/brand_list.html'
+    context_object_name = 'brands'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            title='Производители'
+        )
+        logger.info(f"`{format_name(self.request)}` - Рендеринг шаблона с Brands")
+        return context
