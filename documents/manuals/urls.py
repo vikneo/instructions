2735faf21c77dad35363b7093cd96752ce9b53c7
Viktor <vm-viktor@mail.ru>
2024-11-01 13:08:34 +0700

    add path to template brand_list.html

--- documents/manuals/urls.py
+++ documents/manuals/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 
+from .views import (
+    BrandView
+)
+
 app_name = 'manual'
 
-urlpatterns = []
+urlpatterns = [
+    path("", BrandView.as_view(), name='brand-list'),
+]
